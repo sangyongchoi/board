139e236024ef381cfe6971952d37e43ddaabaeda
sangyongchoi <45090399+sangyongchoi@users.noreply.github.com>
2021-05-14 10:55:37 +0900

    build 폴더 삭제

--- build/tmp/kapt3/stubs/main/com/board/dev/domain/reply/repository/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.board.dev.domain.reply.repository;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u00012\u00020\u0004\u00a8\u0006\u0005"}, d2 = {"Lcom/board/dev/domain/reply/repository/ReplyRepository;", "Lorg/springframework/data/repository/CrudRepository;", "Lcom/board/dev/domain/reply/entity/Reply;", "", "Lcom/board/dev/domain/reply/repository/ReplyRepositoryCustom;", "dev"})
-public abstract interface ReplyRepository extends org.springframework.data.repository.CrudRepository<com.board.dev.domain.reply.entity.Reply, java.lang.Long>, com.board.dev.domain.reply.repository.ReplyRepositoryCustom {
-}
--- build/tmp/kapt3/stubs/main/com/board/dev/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.board.dev.domain.user.repository;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0000\bf\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Lcom/board/dev/domain/user/repository/UserRepository;", "Lorg/springframework/data/repository/CrudRepository;", "Lcom/board/dev/domain/user/entity/User;", "", "dev"})
-public abstract interface UserRepository extends org.springframework.data.repository.CrudRepository<com.board.dev.domain.user.entity.User, java.lang.String> {
-}
